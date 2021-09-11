0f17ce05039f3a58f36f5175bff487d30caa3570
MightyCoderX <mattiaca003@gmail.com>
2021-09-11 22:59:52 +0200

    Fixed mars details and code symbol

--- app.js
+++
@@ -1,4 +0,0 @@
-CSS.paintWorklet.addModule('https://cdnjs.cloudflare.com/ajax/libs/seedrandom/3.0.5/seedrandom.min.js');
-CSS.paintWorklet.addModule('/js/stars.js');
-CSS.paintWorklet.addModule('/js/starship-windows.js');
-
